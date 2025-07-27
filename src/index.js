920ac6defc33bc35753dee6f97d14a8f758dce62
Marc-Antoine Argenton <maargenton.dev@gmail.com>
2025-07-26 20:55:53 -0700

    Run updateBrew() after pre-install-check, restore or install

--- src/index.js
+++ src/index.js
@@ -14,12 +14,17 @@ async function run() {
         const cacheRestored = await brew.restoreCache();
         if (cacheRestored) {
             await brew.setupEnv();
+            await brew.updateBrew();
         } else {
             const isBrewAvailable = await brew.checkAvailability();
             if (!isBrewAvailable) {
+                core.info('Installing Homebrew...');
                 await brew.installBrew();
+            } else {
+                core.info('Homebrew is already installed');
             }
             await brew.setupEnv();
+            await brew.updateBrew();
         }
 
         const packages = core.getInput('packages');
